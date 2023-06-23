5ca38ac99a2a4de0ede945c8ab532c0a67b37261
Yajat Mukhija <yaj@Yajats-MacBook-Pro.local>
2023-06-24 01:02:52 +0530

    init

---
+++ src/components/Experience.jsx
@@ -0,0 +1,46 @@
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import {motion} from 'framer-motion';
+import 'react-vertical-timeline-component/style.min.css';
+
+import {styles} from '../styles';
+import { experiences } from '../constants';
+import {SectionWrapper} from '../hoc';
+import {textVariant} from '../utils/motion';
+
+
+
+const ExperienceCard = ({experience,index}) => {
+  <VerticalTimelineElement
+    contentStyle={{background : '#1d1836' , color:'#fff'}}
+    contentArrowStyle = {{ borderRight: '7px solid #232631'}}
+  >
+  {experience.date}
+
+  </VerticalTimelineElement>
+}
+
+
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div
+      variants={textVariant()}
+      >
+        <p className={styles.sectionSubText + ''}> What I have done so far! </p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map( (experience, index) => (
+            <ExperienceCard key = {index}  experience = {experience}/>
+          ))}
+
+        </VerticalTimeline>
+
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Experience, "work")
