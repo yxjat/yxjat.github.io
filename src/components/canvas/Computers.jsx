e8362e99cf511fc141ab9b2e480909bf984df0f6
Yajat Mukhija <yaj@Yajats-MacBook-Pro.local>
2023-10-02 17:10:39 +0530

    optimised phone

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 1.5}
-        position={isMobile ? [0, -3, -2.2] : [0, -2.25, 0]}
+        position={isMobile ? [0, -1, .2] : [0, -2.25, 0]}
         rotation={[0.1, 0.3, 0]}
       />
     </mesh>
